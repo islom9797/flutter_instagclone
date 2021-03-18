8d09e0082aa27a214ac94c2e4af851a074fc8910
islom9797 <rixsiyevvv@gmail.com>
2021-03-19 03:32:18 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_instagclone/pages/signin_page.dart';
+import 'package:flutter_instagclone/pages/signup_page.dart';
+import 'package:flutter_instagclone/pages/splash_page.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity
+
+      ),
+      home:SplashPage(),
+      routes: {
+        SplashPage.id:(context)=>SplashPage(),
+        SignUpPage.id:(context)=>SignUpPage(),
+        SignInPage.id:(context)=>SignInPage(),
+      },
+    );
+  }
+}
---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_instagclone/pages/signin_page.dart';
+class SplashPage extends StatefulWidget {
+  static final String id="splash_page";
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  _initTimer(){
+    Timer(Duration(seconds: 10),(){
+      _callSignInPage();
+    });
+  }
+  _callSignInPage(){
+    Navigator.pushReplacementNamed(context, SignInPage.id);
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _initTimer();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromRGBO(193, 53, 132, 1),//bu yerda birni manosi hiralashtirishni kodi 1 degani asl rang
+              Color.fromRGBO(131, 58, 180, 1),//bu yerda birni manosi
+            ]
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(
+                child: Center(
+                  child: Text("Instagram",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 45,
+                    fontFamily: "Billabong"
+                  ),
+                  ),
+
+                )
+            ),
+            Text("All rights reserved",style: TextStyle(color: Colors.white,fontSize: 20),),
+            SizedBox(height: 50,)
+
+          ],
+        ),
+      ),
+    );
+  }
+}
